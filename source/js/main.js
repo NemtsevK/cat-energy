8a1fefc1d85dcca4062fe25485a3d29cc74b0138
Konstantin <mutecross@gmail.com>
2024-06-14 14:59:49 +0300

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -2,19 +2,13 @@ const nav = document.querySelector('.nav');
 const navToggle = nav.querySelector('.nav__toggle');
 const mapIframe = document.querySelector('.location__map--nojs');
 
-mapIframe.classList.remove('location__map--nojs');
+mapIframe?.classList.remove('location__map--nojs');
 nav.classList.remove('nav--nojs');
 
-const onNavToggleClick = function () {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
-    nav.classList.add('nav--opened');
-    navToggle.setAttribute('aria-label', 'Закрыть меню');
-  } else {
-    nav.classList.add('nav--closed');
-    nav.classList.remove('nav--opened');
-    navToggle.setAttribute('aria-label', 'Открыть меню');
-  }
+const onNavToggleClick = () => {
+  const isClosed = nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+  navToggle.setAttribute('aria-label', isClosed ? 'Открыть меню' : 'Закрыть меню');
 }
 
 navToggle.addEventListener('click', onNavToggleClick);
